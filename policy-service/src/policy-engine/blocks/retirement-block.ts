290ccf00153ac5486c844aa553de3dc6e792fe6f
palindaa <palinda@xeptagon.com>
2025-05-30 10:00:29 +0100

    Fix validation issue in token id

--- policy-service/src/policy-engine/blocks/retirement-block.ts
+++ policy-service/src/policy-engine/blocks/retirement-block.ts
@@ -178,6 +178,28 @@ export class RetirementBlock {
     }
 
     /**
+     * Get Token
+     * @param ref
+     * @param docs
+     * @private
+     */
+    private async getToken(ref: AnyBlockType, docs: IPolicyDocument[]): Promise<TokenCollection> {
+        let token: TokenCollection;
+        if (ref.options.useTemplate) {
+            if (docs[0].tokens) {
+                const tokenId = docs[0].tokens[ref.options.template];
+                token = await ref.databaseServer.getToken(tokenId, ref.dryRun);
+            }
+        } else {
+            token = await ref.databaseServer.getToken(ref.options.tokenId);
+        }
+        if (!token) {
+            throw new BlockActionError('Bad token id', ref.blockType, ref.uuid);
+        }
+        return token;
+    }
+
+    /**
      * Run action
      * @event PolicyEventType.Run
      * @param {IPolicyEvent} event
@@ -193,15 +215,12 @@ export class RetirementBlock {
     async runAction(event: IPolicyEvent<IPolicyEventState>) {
         const ref = PolicyComponentsUtils.GetBlockRef(this);
 
-        const token = await ref.databaseServer.getToken(ref.options.tokenId);
-        if (!token) {
-            throw new BlockActionError('Bad token id', ref.blockType, ref.uuid);
-        }
-
         const docs = PolicyUtils.getArray<IPolicyDocument>(event.data.data);
         if (!docs.length && docs[0]) {
             throw new BlockActionError('Bad VC', ref.blockType, ref.uuid);
         }
+        
+        const token = await this.getToken(ref, docs);
 
         const docOwner = await PolicyUtils.getDocumentOwner(ref, docs[0], event?.user?.userId);
         if (!docOwner) {
